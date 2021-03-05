e9c5bc4a2e6c4b56d8523cc3250e617fb9514b62
Giap Viet Duc <g.v.duc@azoom.jp>
2021-03-05 17:18:26 +0700

    Add header component

--- components/component-header.vue
+++ components/component-header.vue
@@ -1,5 +1,116 @@
 <template>
-  <div class="header-component">Header</div>
+  <div class="header-component">
+    <div class="header">
+      <div class="headerContainer">
+        <div class="headerWrapper">
+          <div class="left">
+            <div class="logoContainer">
+              <a class="logo" href="/">
+                <svg
+                  width="115"
+                  height="40"
+                  viewBox="0 0 115 40"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <defs>
+                    <linearGradient
+                      x1="92.129%"
+                      y1="100%"
+                      x2="0%"
+                      y2="6.503%"
+                      id="linearGradient-1"
+                    >
+                      <stop stop-color="#2AF598" offset="0%"></stop>
+                      <stop stop-color="#0049FD" offset="100%"></stop>
+                    </linearGradient>
+                  </defs>
+                  <g fill="none" fill-rule="evenodd">
+                    <g>
+                      <rect
+                        fill="url(#linearGradient-1)"
+                        fill-rule="nonzero"
+                        width="40"
+                        height="40"
+                        rx="20"
+                      ></rect>
+                      <g fill-rule="nonzero" fill="#FFF">
+                        <path
+                          d="M32.99 16.072c-.044 2.099-.663 4.06-1.587 5.93-1.632 3.299-3.859 6.13-6.93 8.261-1.182.821-2.453 1.473-3.912 1.684-2.345.337-3.986-.95-4.225-3.274-.216-2.099.287-4.069 1.113-5.966 1.665-3.83 4.132-7.045 7.663-9.407 1.07-.715 2.228-1.248 3.521-1.468 2.183-.372 3.815.713 4.232 2.85.09.456.161.919.126 1.39z"
+                        ></path>
+                        <path
+                          d="M16.257 31.08c-1.495-.387-2.79-1.127-3.984-2.016-2.381-1.77-4.174-3.934-4.994-6.707-.306-1.036-.397-2.088-.087-3.144.49-1.674 1.955-2.748 3.814-2.842 1.602-.082 3.085.293 4.509.93.83.37 1.597.834 2.345 1.332.31.207.37.36.156.684-1.144 1.72-2.025 3.548-2.51 5.527-.324 1.328-.462 2.663-.179 4.015.158.776.504 1.51 1.013 2.142l-.083.079zM20 11.76c-.002 1.515-1.233 2.746-2.761 2.76-1.528.013-2.782-1.196-2.81-2.711A2.771 2.771 0 0 1 17.186 9a2.81 2.81 0 0 1 1.983.802c.528.517.827 1.222.831 1.958z"
+                        ></path>
+                      </g>
+                    </g>
+                    <path
+                      d="M53.654 27.68V13.424h3.52v-1.848h-8.976v1.848h3.542V27.68h1.914zm6.929-13.706c.389 0 .719-.136.99-.407.271-.271.407-.601.407-.99s-.136-.719-.407-.99a1.36 1.36 0 0 0-1.001-.407c-.381 0-.708.14-.979.418a1.359 1.359 0 0 0-.407.979c0 .374.136.7.407.979.271.279.601.418.99.418zm.913 13.706V15.69H59.67v11.99h1.826zm5.62 0v-4.754c.01-1.412.079-2.365.209-2.858.14-.528.392-1.027.759-1.496.367-.47.807-.836 1.32-1.1a3.403 3.403 0 0 1 1.573-.396c.535 0 .99.08 1.364.242.374.161.682.407.924.737s.425.733.55 1.21c.125.477.187 1.375.187 2.695v5.72h1.804v-6.16c0-1.628-.176-2.834-.528-3.619a4.148 4.148 0 0 0-1.584-1.848 4.298 4.298 0 0 0-2.354-.671c-1.628 0-3.036.704-4.224 2.112V15.69H65.29v11.99h1.826zm14.42 0v-4.754c.01-1.412.079-2.365.209-2.858.14-.528.392-1.027.759-1.496.367-.47.807-.836 1.32-1.1a3.49 3.49 0 0 1 1.617-.396c.565 0 1.049.106 1.452.319.403.213.722.52.957.924.235.403.385.832.451 1.287.066.455.099 1.24.099 2.354v5.72h1.826v-6.16c0-1.628-.176-2.834-.528-3.619a4.148 4.148 0 0 0-1.584-1.848 4.298 4.298 0 0 0-2.354-.671c-1.628 0-3.036.704-4.224 2.112V11.18H79.71v16.5h1.826zm15.982.396c.367 0 .902-.059 1.606-.176v-1.562h-1.056c-.352 0-.572-.033-.66-.099-.088-.066-.132-.253-.132-.561v-8.404h2.376V15.69h-2.376v-4.51H95.45v4.51h-2.068v1.584h2.068v8.646c0 .748.169 1.294.506 1.639.337.345.858.517 1.562.517zm10.746 0c1.305 0 2.427-.304 3.366-.913.939-.609 1.753-1.551 2.442-2.827l-1.54-.792c-1.012 1.892-2.479 2.838-4.4 2.838-1.13 0-2.101-.4-2.915-1.199-.814-.8-1.258-1.859-1.331-3.179h10.516c-.044-2.053-.653-3.67-1.826-4.851-1.173-1.18-2.633-1.771-4.378-1.771s-3.208.623-4.389 1.87c-1.18 1.247-1.771 2.743-1.771 4.488 0 1.745.587 3.238 1.76 4.477 1.173 1.24 2.662 1.859 4.466 1.859zm4.114-7.678h-8.272c.675-2.17 2.039-3.256 4.092-3.256 1.012 0 1.907.286 2.684.858.777.572 1.276 1.371 1.496 2.398z"
+                      stroke="var(--text-dark)"
+                      stroke-width=".2"
+                      fill="var(--text-dark)"
+                      fill-rule="nonzero"
+                    ></path>
+                  </g>
+                </svg>
+              </a>
+            </div>
+            <div class="search"></div>
+          </div>
+          <div class="middle">
+            <div class="menuWrapper">
+              <div class="forum"></div>
+              <div class="video"></div>
+              <div class="podcasts"></div>
+            </div>
+          </div>
+          <div class="right"></div>
+        </div>
+      </div>
+    </div>
+    <div class="mainPage">
+      <div class="tabContainer">
+        <div class="tabs">
+          <button class="tab">Home</button>
+          <button class="tab">ForU</button>
+        </div>
+      </div>
+      <div class="pageLayout">
+        <div class="pageContainer">
+          <div class="body">
+            <div class="left">
+              <div class="storiesWrapper">
+                <div class="header"></div>
+                <div class="storiesContainer">
+                  <div class="newStory">
+                    <button class="newStoryButton"></button>
+                  </div>
+                  <div class="item"></div>
+                </div>
+              </div>
+            </div>
+            <div class="right">
+              <div class="myInfo">
+                <div class="header"></div>
+                <div class="body">
+                  <div class="top"></div>
+                  <div class="message"></div>
+                  <div class="bottom">
+                    <div class="stats">
+                      <span class="label"></span>
+                      <span class="label"></span>
+                    </div>
+                    <div class="rank">
+                      <span class="label"></span>
+                      <span class="label"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,7 +118,58 @@ export default {}
 </script>
 
 <style>
+html {
+  --text-dark: #222b45;
+}
 .header-component {
   font-size: 16px;
+  width: 100%;
+}
+.header-component > .header {
+  box-shadow: 0 2px 4px 0 rgba(182, 182, 182, 0.5);
+  background-color: #fff;
+  z-index: 1000;
+  position: relative;
+}
+.header-component > .header > .headerContainer {
+  max-width: 1144px;
+  padding: 0 20px;
+  width: 100%;
+  margin: 0 auto;
+}
+.header-component > .header > .headerContainer > .headerWrapper {
+  display: flex;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding-top: 8px;
+  padding-bottom: 8px;
+}
+.header-component > .header > .headerContainer > .headerWrapper > .left {
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  flex: 0 0 auto;
+  padding-right: 0;
+  width: auto;
+  padding-left: 20px;
+}
+.header-component
+  > .header
+  > .headerContainer
+  > .headerWrapper
+  > .left
+  > .logoContainer {
+  display: flex;
+}
+.header-component
+  > .header
+  > .headerContainer
+  > .headerWrapper
+  > .left
+  > .logoContainer
+  > .logo {
+  text-decoration: none;
+  cursor: pointer;
 }
 </style>
